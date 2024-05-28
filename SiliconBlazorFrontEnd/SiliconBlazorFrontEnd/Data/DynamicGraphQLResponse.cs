20e7f3e14a52ac7c7996085b5c0072f52799f4d6
simon <simonelenbrink@gmail.com>
2024-05-28 17:27:13 +0200

    GraphQL Courses added

---
+++ SiliconBlazorFrontEnd/SiliconBlazorFrontEnd/Data/DynamicGraphQLResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SiliconBlazorFrontEnd.Data;
+
+public class DynamicGraphQLResponse
+{
+    [JsonPropertyName("data")]
+    public JsonElement Data { get; set; }
+}
---
+++ SiliconBlazorFrontEnd/SiliconBlazorFrontEnd/Data/GraphQLQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace SiliconBlazorFrontEnd.Data
+{
+    public class GraphQLQuery
+    {
+        public string Query { get; set; } = null!;
+    }
+}
